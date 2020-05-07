633a89b188fd293739ab24eb31532345c8f65608
maschad <chad.nehemiah94@gmail.com>
2020-05-06 23:06:58 -0500

    contract complete time for tests

--- contracts/StarNotary.sol
+++ contracts/StarNotary.sol
@@ -14,14 +14,6 @@ contract StarNotary is ERC721 {
         string name;
     }
 
-    // Implement Task 1 Add a name and symbol properties
-    // name: Is a short name to your token
-    // symbol: Is a short string like 'USD' -> 'American Dollar'
-    constructor(string memory _name, string memory _symbol) public {
-        name = _name;
-        symbol = _symbol;
-    }
-
     // mapping the Star with the Owner Address
     mapping(uint256 => Star) public tokenIdToStarInfo;
     // mapping the TokenId and price
@@ -69,7 +61,7 @@ contract StarNotary is ERC721 {
         returns (string memory)
     {
         //1. You should return the Star saved in tokenIdToStarInfo mapping
-        return tokenIdToStarInfo[_tokenId];
+        return tokenIdToStarInfo[_tokenId].name;
     }
 
     // Implement Task 1 Exchange Stars function
@@ -82,11 +74,11 @@ contract StarNotary is ERC721 {
         );
         //2. You don't have to check for the price of the token (star)
         //3. Get the owner of the two tokens (ownerOf(_tokenId1), ownerOf(_tokenId1)
-        ownerToken1 = ownerOf(_tokenId1);
-        onwerToken2 = ownerOf(_tokenId2);
+        address ownerToken1 = ownerOf(_tokenId1);
+        address onwerToken2 = ownerOf(_tokenId2);
         //4. Use _transferFrom function to exchange the tokens.
-        transferFrom(owner1, owner2, _tokenId1);
-        transferFrom(owner2, owner1, _tokenId2);
+        transferFrom(ownerToken1, onwerToken2, _tokenId1);
+        transferFrom(onwerToken2, ownerToken1, _tokenId2);
     }
 
     // Implement Task 1 Transfer Stars
